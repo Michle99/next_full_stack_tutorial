2fb7ebae331c32dfff55e500c09c040f3667b0d0
Michle99 <sammiyonyx@gmail.com>
2023-12-26 20:17:30 -0500

    Root Layout and Nested Layout

--- next_supabase_app/api.ts
+++ next_supabase_app/api.ts
@@ -1,11 +1,6 @@
-import { SupabaseClient, createClient } from "@supabase/supabase-js";
-import { Database } from "./types/supabase";
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient<Database>(
-    supabaseUrl,
-    supabaseKey
-)
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+);
---
+++ next_supabase_app/app/(posts)/page.tsx
@@ -0,0 +1,9 @@
+import "../globals.css"
+
+export default function Index() {
+  return (
+    <main>
+      <h3 className="text-center mt-4">Post Home</h3>
+    </main>
+  );
+}
