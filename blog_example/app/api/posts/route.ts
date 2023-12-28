dd25b4d753f93bef65fb81086ef5a2e0146b1bc6
Michle99 <sammiyonyx@gmail.com>
2023-12-28 16:25:36 -0500

    add `api/posts/route.ts` to fetch posts

---
+++ blog_example/app/api/posts/route.ts
@@ -0,0 +1,17 @@
+/**
+ * Retrieves a list of posts from the api dummyjson.
+ * @returns {Promise<Object>} A promise that resolves to an object containing 
+ * the posts data.
+ */
+
+
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+   const response = await fetch("https://dummyjson.com/posts", {
+    next: { revalidate: 60 },
+   });
+   const data = await response.json();
+
+   return NextResponse.json(data);
+}
