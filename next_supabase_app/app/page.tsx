b616b859b1b90bf0cf7a2c9f5b15af04de0e7cf8
Michle99 <sammiyonyx@gmail.com>
2023-12-31 00:18:13 -0500

    added supabase server to `app/page.tsx`

--- next_supabase_app/app/page.tsx
+++ next_supabase_app/app/page.tsx
@@ -1,5 +1,15 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data: { user }} = await supabase.auth.getUser();
+
+  if (!user) redirect('/sign-in');
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h3>Home Page...</h3>
