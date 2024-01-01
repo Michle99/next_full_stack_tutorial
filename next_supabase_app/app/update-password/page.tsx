6798d0229bf95c3a116c322bf0e56751197cfe1a
Michle99 <sammiyonyx@gmail.com>
2024-01-01 18:56:11 -0500

    updated update-password page

--- next_supabase_app/app/update-password/page.tsx
+++ next_supabase_app/app/update-password/page.tsx
@@ -1,2 +1,15 @@
+import UpdatedPassword from "@/components/Auth/UpdatePassword";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
+
+export default async function UpdatePassword () {
+    const supabase = createServerComponentClient({ cookies });
+
+    const { data: { session }} = await supabase.auth.getSession();
+
+    if (!session) redirect('/sign-in')
+
+    return <UpdatedPassword/>
+}
