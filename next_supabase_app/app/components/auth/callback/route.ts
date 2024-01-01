8bcbd60d1a9192cbe6b08824585acaf9be20bb87
Michle99 <sammiyonyx@gmail.com>
2023-12-31 23:36:45 -0500

    updated `auth/callback/route.ts` to store user session during authentication flow

--- next_supabase_app/app/components/auth/callback/route.ts
+++ next_supabase_app/app/components/auth/callback/route.ts
@@ -6,4 +6,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET (request: NextRequest) {
     const requestURL = new URL(request.url);
     const code = requestURL.searchParams.get('code');
+
+    if (code) {
+        const supabase = createRouteHandlerClient({ cookies });
+        await supabase.auth.exchangeCodeForSession(code);
+    }
+    
+    // URL to redirect to after sign in process completes
+    return NextResponse.redirect(requestURL.origin);
 }
