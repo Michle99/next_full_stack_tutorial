788b6b6f6bafa7bbe42a495ee58a0c1a11e3c739
Michle99 <sammiyonyx@gmail.com>
2024-01-01 19:01:32 -0500

    updated `app/page.tsx`

--- next_supabase_app/app/page.tsx
+++ next_supabase_app/app/page.tsx
@@ -1,6 +1,7 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import Link from "next/link";
 
 export default async function Home() {
 
@@ -11,8 +12,13 @@ export default async function Home() {
   if (!user) redirect('/sign-in');
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>Home Page...</h3>
+    <main className="card">
+      <h2>Home Page...</h2>
+      <code className="highlight">{user.role}</code>
+      <Link className="button" href="/profile">
+        Go to Profile
+      </Link>
+      {/* <SignOut/> */}
     </main>
   );
 }
